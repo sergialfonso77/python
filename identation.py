bc2fe150e5c68ff617e9d3064561ffd384f23795
unknown <sergialfonso7@gmail.com>
2024-12-10 11:09:09 +0100

    identation

---
+++ identation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+""" Probar identación python """
+A = 1
+if  A == 1: 
+    print("A es 1")
+    print("Test A es 1")
+else:
+    print("A no es 1")
+    print("Test a no es 1")
+
+print("Esto siempre se imprime") 
