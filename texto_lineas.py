dc4b48b2939e6978564e5bc560233b28676d18d9
unknown <sergialfonso7@gmail.com>
2024-12-11 09:25:50 +0100

    texto_lineas

---
+++ texto_lineas.py
@@ -0,0 +1,19 @@
+
+texto_largo = ["Linea " + str(i+1) for i in range(1000)]
+
+lineas = 1000/25
+lineas_por_bloque = 25
+contador_global = 0
+contador = 0
+
+bloque_actual = []
+bloques = []
+
+for linea in texto_largo: 
+    bloque_actual.append(linea)
+
+    if (len(bloque_actual) == lineas_por_bloque):
+        bloques.append(bloque_actual)
+        bloque_actual = []
+        contador_global += 1
+        print(f"\nBloque {contador_global} de {lineas}")
